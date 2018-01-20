08e5ccfb356c7bc145dea7e30b4d951b01a3a988
Ara <ayaghsizian@gmail.com>
2018-01-19 21:17:31 -0500

    added an NUnit testcase testing the Add method

---
+++ FirstStudio/MyTestCase.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstStudio
+{
+    [TestFixture]
+    class MyTestCase
+    {
+        [TestCase]
+        public void Add()
+        {
+            MyMath math = new MyMath();
+            Assert.AreEqual(77, math.Add(32, 45));
+        }
+    }
+}
